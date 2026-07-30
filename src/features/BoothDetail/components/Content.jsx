7a6d03338b0c21c0e7b756f0016621e70388fa1e
choidaye8170 <gracechoidaye@gmail.com>
2026-07-30 20:08:02 +0900

    refactor : content 오류 수정 및 booths.js 오타수정

--- src/features/BoothDetail/components/Content.jsx
+++ src/features/BoothDetail/components/Content.jsx
@@ -1,64 +1,31 @@
-import Header from "./Header";
-import Links from "./Links";
-import MainCard from "./MainCard";
-import Content from "./Content";
-
-export default function BoothDetail({ booth, isOpen, setIsOpen }) {
+export default function BoothDetail({ booth }) {
   return (
-    <div
-      className={`
-        absolute bottom-0 left-0 w-full
-        flex flex-col
-        rounded-t-[0.625rem]
-        z-100
-        bg-[#010101]
-        px-[1.31rem]
-        text-white
-        transition-all duration-300 ease-in-out
-        ${isOpen ? "h-screen" : "h-[26rem]"}
-      `}
-    >
-      {/* Header 고정 */}
-      <div className="shrink-0 w-full bg-[#010101]">
-        <button
-          onClick={() => setIsOpen(!isOpen)}
-          className="w-full h-[2rem] flex justify-center pt-[0.6rem]"
-        >
-          <div className="w-[4.375rem] h-[0.25rem] rounded-full bg-[#363636]" />
-        </button>
+    <div className="text-[0.75rem] mt-[1rem] mb-[2.3rem] font-extralight">
+      <div>{booth.content}</div>
 
-        <Header booth={booth} />
+      <div className="mt-[1rem]">
+        <div className="text-[1rem] font-medium">주요기능</div>
+
+        <ul className="list-disc pl-[1rem]">
+          {booth.function.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
       </div>
 
-      {/* 스크롤 영역 */}
-      <div
-        className="
-          relative
-          flex-1
-          min-h-0
-          w-full
-          overflow-y-auto
-          scrollbar-hide
-          overscroll-contain
-          mt-[1rem]
-        "
-      >
-        <MainCard booth={booth} />
-        <Links booth={booth} />
-        <Content booth={booth} />
+      <div className="mt-[1rem]">
+        <div className="text-[1rem] font-medium">기술 스택</div>
 
-        {/* 하단 그라데이션 */}
-        <div
-          className="
-            absolute
-            bottom-0
-            left-0
-            w-full
-            h-[4.5rem]
-            pointer-events-none
-            bg-[linear-gradient(180deg,rgba(0,0,0,0)_0%,#000_80%)]
-          "
-        />
+        <ul className="list-disc pl-[1rem]">
+          {booth.techstack.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="mt-[1rem]">
+        <div className="text-[1rem] font-medium">회고</div>
+        <div>{booth.retrospect}</div>
       </div>
     </div>
   );
