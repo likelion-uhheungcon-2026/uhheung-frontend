8a0f85b5f81e2a8547ee296219ab11c5e7c8f346
choidaye8170 <gracechoidaye@gmail.com>
2026-08-02 13:54:30 +0900

    refactor : BoothDetail 가로 드래그 수정

--- src/features/BoothDetail/components/MainCard.jsx
+++ src/features/BoothDetail/components/MainCard.jsx
@@ -1,6 +1,6 @@
 export default function MainCard({ booth }) {
   return (
-    <div className="relative w-[22.5rem] h-[12.3125rem] overflow-hidden rounded-t-[0.625rem]">
+    <div className="relative w-full h-[12.3125rem] overflow-hidden rounded-t-[0.625rem]">
       <img src={booth.serviceimage} className="w-full h-full object-cover" />
 
       {/* 아래쪽 어둡게 그라데이션 */}
